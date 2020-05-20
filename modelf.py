ef46ca5a74078dc16b6b0455b44b6ff3490a6c17
ab-10 <armins.bagrats@gmail.com>
2020-05-20 07:13:50 +0100

    implement f

--- modelf.py
+++ modelf.py
@@ -1,6 +1,9 @@
+import torch
 import torch.nn as nn
 
-LATENT_CODE_SIZE = 128
+LATENT_CODE_SIZE = 128 # size of the Z vector
+SDF_NET_BREADTH = 256 # size of the w vector
+
 amcm = 24 # Autoencoder Model Complexity Multiplier
 
 class Lambda(nn.Module):
@@ -15,27 +18,21 @@ class ModelF(nn.Module):
     def __init__(self):
         super(ModelF, self).__init__()
         self.add_module('encoder', nn.Sequential(
-            nn.Conv3d(in_channels = 1, out_channels = 1 * amcm, kernel_size = 4, stride = 2, padding = 1),
-            nn.BatchNorm3d(1 * amcm),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Linear(in_features = 3 + LATENT_CODE_SIZE, out_features = SDF_NET_BREADTH),
+            nn.ReLU(inplace=True),
 
-            nn.Conv3d(in_channels = 1 * amcm, out_channels = 2 * amcm, kernel_size = 4, stride = 2, padding = 1),
-            nn.BatchNorm3d(2 * amcm),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Linear(in_features = SDF_NET_BREADTH, out_features = SDF_NET_BREADTH),
+            nn.ReLU(inplace=True),
 
-            nn.Conv3d(in_channels = 2 * amcm, out_channels = 4 * amcm, kernel_size = 4, stride = 2, padding = 1),
-            nn.BatchNorm3d(4 * amcm),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
+            nn.Linear(in_features = SDF_NET_BREADTH, out_features = SDF_NET_BREADTH),
+            nn.ReLU(inplace=True),
 
-            nn.Conv3d(in_channels = 4 * amcm, out_channels = LATENT_CODE_SIZE * 2, kernel_size = 4, stride = 1),
-            nn.BatchNorm3d(LATENT_CODE_SIZE * 2),
-            nn.LeakyReLU(negative_slope=0.2, inplace=True),
-
-            Lambda(lambda x: x.reshape(x.shape[0], -1)),
-
-            nn.Linear(in_features = LATENT_CODE_SIZE * 2, out_features=LATENT_CODE_SIZE)
+            nn.Linear(in_features = SDF_NET_BREADTH, out_features = SDF_NET_BREADTH),
+            nn.ReLU(inplace=True) 
         ))
         self.cuda()
 
-    def forward(self, x):
+    def forward(self, points, latent_codes):
+        x = torch.cat((points, latent_codes), dim=1)
+        x = x.cuda()
         return self.encoder(x)
